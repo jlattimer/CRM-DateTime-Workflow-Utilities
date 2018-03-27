44c4e9e37f03e593f4c99e5d924548178443d620
Gareth Kear <gareth.kear@warp.co.uk>
2018-03-27 14:06:02 +0100

    Created workflow activity

--- LAT.WorkflowUtilities.DateTimes/GetNumberOfBusinessDays.cs
+++ LAT.WorkflowUtilities.DateTimes/GetNumberOfBusinessDays.cs
@@ -1,11 +1,29 @@
 ﻿using System;
 using System.Activities;
+using LAT.WorkflowUtilities.DateTimes.Common;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
 
 namespace LAT.WorkflowUtilities.DateTimes
 {
     public class GetNumberOfBusinessDays : WorkFlowActivityBase
     {
         public GetNumberOfBusinessDays() : base(typeof(GetNumberOfBusinessDays)) { }
+
+        [RequiredArgument]
+        [Input("Start Date For Range")]
+        public InArgument<DateTime> StartDate { get; set; }
+
+        [RequiredArgument]
+        [Input("End Date For Range")]
+        public InArgument<DateTime> EndDate { get; set; }
+
+        [Input("Holiday/Closure Calendar")]
+        [ReferenceTarget("calendar")]
+        public InArgument<EntityReference> HolidayClosureCalendar { get; set; }
+
+        [Output("Number Of Business Days")]
+        public OutArgument<int> NumberOfBusinessDays { get; set; }
 
         protected override void ExecuteCrmWorkFlowActivity(CodeActivityContext context, LocalWorkflowContext localContext)
         {
@@ -14,6 +32,29 @@ namespace LAT.WorkflowUtilities.DateTimes
             if (localContext == null)
                 throw new ArgumentNullException(nameof(localContext));
 
+            try
+            {
+                var dateToCheck = StartDate.Get(context);
+                var endDate = EndDate.Get(context);
+
+                var businessDays = 0;
+                while (dateToCheck <= endDate)
+                {
+                    if (dateToCheck.IsBusinessDay(localContext.OrganizationService,
+                        this.HolidayClosureCalendar.Get(context)))
+                    {
+                        businessDays++;
+                    }
+
+                    dateToCheck = dateToCheck.AddDays(1);
+                }
+
+                this.NumberOfBusinessDays.Set(context, businessDays);
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidPluginExecutionException(OperationStatus.Failed, $"{ex.Message}");
+            }
         }
     }
 }
