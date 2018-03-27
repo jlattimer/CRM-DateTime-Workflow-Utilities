01de386353710794858d3733ca7fc04bc68941c9
Gareth Kear <gareth.kear@warp.co.uk>
2018-03-27 13:17:21 +0100

    Added bare bones workflow activity

---
+++ LAT.WorkflowUtilities.DateTimes.Tests/GetNumberOfBusinessDaysTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LAT.WorkflowUtilities.DateTimes.Tests
+{
+    [TestClass]
+    public class GetNumberOfBusinessDaysTests
+    {
+        #region Test Initialization and Cleanup
+        // Use ClassInitialize to run code before running the first test in the class
+        [ClassInitialize()]
+        public static void ClassInitialize(TestContext testContext) { }
+
+        // Use ClassCleanup to run code after all tests in a class have run
+        [ClassCleanup()]
+        public static void ClassCleanup() { }
+
+        // Use TestInitialize to run code before running each test 
+        [TestInitialize()]
+        public void TestMethodInitialize() { }
+
+        // Use TestCleanup to run code after each test has run
+        [TestCleanup()]
+        public void TestMethodCleanup() { }
+        #endregion
+    }
+}
---
+++ LAT.WorkflowUtilities.DateTimes/GetNumberOfBusinessDays.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Activities;
+
+namespace LAT.WorkflowUtilities.DateTimes
+{
+    public class GetNumberOfBusinessDays : WorkFlowActivityBase
+    {
+        public GetNumberOfBusinessDays() : base(typeof(GetNumberOfBusinessDays)) { }
+
+        protected override void ExecuteCrmWorkFlowActivity(CodeActivityContext context, LocalWorkflowContext localContext)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            if (localContext == null)
+                throw new ArgumentNullException(nameof(localContext));
+
+        }
+    }
+}
